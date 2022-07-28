3b7333f85b75ba93e8ffc6f55552df810babf4d6
remyduijn <remy.duijn@gmail.com>
2022-07-28 12:00:10 -0300

    new edit

---
+++ src/pages/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from '../components/Gallery';
+import ScrollToTop from '../components/ScrollToTop';
+
+function GalleryPage() {
+  return (
+    <>
+      <ScrollToTop />
+      <Contact />
+    </>
+  );
+}
+
+export default GalleryPage;
